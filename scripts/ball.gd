cd3645c791b06e34431624d4da5694749f7b946d
Christian Bouwense <bouwensec@gmail.com>
2023-07-04 11:46:38 -0400

    got it kinda bouncing correctly?

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,9 +1,20 @@
 extends StaticBody3D
 
 @export var speed = 10
+var velocity = Vector3(1.0, 0.0, 0.0)
 
 func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	move_and_collide(speed * delta * Vector3(1.0, 0.0, 0.0))
+	var collision = move_and_collide(speed * delta * velocity)
+	
+	if collision:
+		print(collision)
+		print(collision.get_remainder())
+		print(collision.get_normal())
+#		velocity = collision.get_remainder().bounce(collision.get_normal())
+		velocity = collision.get_normal()
+		print(velocity)
+
+		move_and_collide(velocity)
