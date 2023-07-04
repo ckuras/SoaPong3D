483174f268a65490bd27005e7671d1877fb91e3b
ckuras <kuras.chris@gmail.com>
2023-07-04 12:18:09 -0400

    Added boinge

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -3,18 +3,9 @@ extends StaticBody3D
 @export var speed = 10
 var velocity = Vector3(1.0, 0.0, 0.0)
 
-func _ready():
-	pass # Replace with function body.
-
 func _physics_process(delta):
 	var collision = move_and_collide(speed * delta * velocity)
 	
 	if collision:
-		print(collision)
-		print(collision.get_remainder())
-		print(collision.get_normal())
-#		velocity = collision.get_remainder().bounce(collision.get_normal())
-		velocity = collision.get_normal()
-		print(velocity)
-
-		move_and_collide(velocity)
+		var collider_position = collision.get_collider().position
+		velocity = (position - collider_position).normalized()
